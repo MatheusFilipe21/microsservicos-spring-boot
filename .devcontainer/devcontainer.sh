048a7c7284cf53f8cdfc83cd14bef812a5013064
Matheus Pereira <matheusfnpereira@gmail.com>
2025-04-08 07:21:20 -0300

    Criando uma rede para conectar os containers

--- .devcontainer/devcontainer.sh
+++ .devcontainer/devcontainer.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+echo "Iniciando configuração do ambiente de desenvolvimento..."
+
+echo "Verificando se a rede dev-network existe..."
+docker network ls | grep dev-network >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo "Rede dev-network já existe."
+else
+    echo "Criando rede dev-network..."
+    docker network create dev-network
+fi
+
 echo "Iniciando containers..."
 docker compose -f .devcontainer/docker-compose.yml down
 docker compose -f .devcontainer/docker-compose.yml up -d
